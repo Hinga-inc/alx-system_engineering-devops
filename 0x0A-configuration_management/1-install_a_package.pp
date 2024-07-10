cb996096d1411af239f5ed1ac2b49e9a930e1b48
Hinga Peter <hingapeter18@gmail.com>
2024-07-10 12:39:11 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # this is a SDL script that intalls a package flask from pip3 of version 2.1.0
 package { 'werkzeug':
-  ensure   => 'installed',
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
 package { 'flask':
