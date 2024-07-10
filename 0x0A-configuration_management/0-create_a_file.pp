2581a7b0b0355cb699ce1850450842daddbc4ce7
Hinga Peter <hingapeter18@gmail.com>
2024-07-10 10:24:03 +0300

    0- create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,6 +3,6 @@ file { '/tmp/school':
   ensure  => 'file',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love puppet',
-  mode    => '0744'
+  mode    => '0744',
+  content => 'I love puppet'
 }
