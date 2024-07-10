95d83b3d98fd2c6ac4f9423216348715334cd2e2
Hinga Peter <hingapeter18@gmail.com>
2024-07-10 10:26:42 +0300

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # this is a puppet sdl script that creates a new file name and ensures it is a file and creates the perissions owner group ant it content
 file { '/tmp/school':
   ensure  => 'file',
+  content => 'I love puppet',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
-  content => 'I love puppet'
+  mode    => '0744'
 }
