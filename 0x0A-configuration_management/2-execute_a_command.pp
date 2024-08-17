0aec6f6face6f595789ea9d3e1ecb79458873dd2
Hinga Peter <hingapeter18@gmail.com>
2024-08-17 11:38:33 +0300

    execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # this is puppet sdl script that executes a command that kills a process
-exec { 'killmenow':
-  command     => '/usr/bin/pkill -f killmenow',
-  refreshonly => true
+exec { 'pkill':
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin'
 }
