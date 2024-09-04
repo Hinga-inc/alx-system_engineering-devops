302c7d1dd4544f7f75159c5ef23b3a55aefef5c4
Hinga Peter <hingapeter18@gmail.com>
2024-09-04 11:01:12 +0300

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,31 @@
-# make changes to config file using Puppet
+# Puppet manifest to configure SSH client for kamau-hinga
 
-include stdlib
-
-file_line { 'Refuse to authenticate using a password':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+# Ensure the SSH config directory exists
+file { '/home/kamau-hinga/.ssh':
+  ensure => directory,
+  owner  => 'kamau-hinga',
+  group  => 'kamau-hinga',
+  mode   => '0700',
 }
 
-file_line { 'Use private key':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school'
+# Ensure the SSH config file exists
+file { '/home/kamau-hinga/.ssh/config':
+  ensure => file,
+  owner  => 'kamau-hinga',
+  group  => 'kamau-hinga',
+  mode   => '0600',
+}
+
+# Add the IdentityFile configuration
+file_line { 'Declare identity file':
+  path  => '/home/kamau-hinga/.ssh/config',
+  line  => '    IdentityFile /home/kamau-hinga/.ssh/school',
+  match => '^    IdentityFile',
+}
+
+# Disable password authentication
+file_line { 'Turn off passwd auth':
+  path  => '/home/kamau-hinga/.ssh/config',
+  line  => '    PasswordAuthentication no',
+  match => '^    PasswordAuthentication',
 }
